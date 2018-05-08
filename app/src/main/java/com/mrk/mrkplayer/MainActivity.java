641a932ea0ae61232c867a62bac4e564d7755989
mrk <1152574267@qq.com>
2018-05-08 14:03:47 +0800

    添加底部导航栏

--- app/src/main/java/com/mrk/mrkplayer/MainActivity.java
+++ app/src/main/java/com/mrk/mrkplayer/MainActivity.java
@@ -1,60 +1,112 @@
 package com.mrk.mrkplayer;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 
-import com.mrk.mrkplayer.decoration.MyDecoration;
-import com.mrk.mrkplayer.adapter.XRecyclerViewAdapter;
-import com.mrk.mrkplayer.bean.TabItem;
+import com.mrk.mrkplayer.model.FragmentGenerator;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity implements XRecyclerViewAdapter.OnItemClickListener, XRecyclerViewAdapter.OnItemLongClickListener {
+public class MainActivity extends FragmentActivity implements /*XRecyclerViewAdapter.OnItemClickListener, XRecyclerViewAdapter.OnItemLongClickListener
+        , */TabLayout.OnTabSelectedListener {
+    private TabLayout mTabLayout;
+    private List<Fragment> mFragmentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
         initData();
     }
 
+    private void initView() {
+        mTabLayout = (TabLayout) findViewById(R.id.bottom_tab_list);
+
+        mFragmentList = FragmentGenerator.getFragmentList();
+
+        mTabLayout.addOnTabSelectedListener(this);
+    }
+
     private void initData() {
-        RecyclerView tabList = (RecyclerView) findViewById(R.id.tablist);
+//        RecyclerView tabList = (RecyclerView) findViewById(R.id.tablist);
+//
+//        GridLayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 1, GridLayoutManager.VERTICAL, false);
+//        tabList.setLayoutManager(layoutManager);
 
-        GridLayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 1, GridLayoutManager.VERTICAL, false);
-        tabList.setLayoutManager(layoutManager);
-
-        // 创建数据集
-        int[] arr = new int[]{R.string.tab_video, R.string.tab_music, R.string.tab_dictionary};
-        int[] drawableArr = new int[]{R.drawable.tab_video, R.drawable.tab_music, R.drawable.tab_dictionary};
-        List<TabItem> mDataList = new ArrayList<TabItem>();
+//        List<TabItem> mDataList = new ArrayList<TabItem>();
         for (int i = 0; i < 3; i++) {
-            TabItem item = new TabItem();
-            item.setTv(getResources().getString(arr[i]));
-            item.setIcon(drawableArr[i]);
-            mDataList.add(item);
+//            TabItem item = new TabItem();
+//            item.setTv(getResources().getString(arr[i]));
+//            item.setIcon(drawableArr[i]);
+//            mDataList.add(item);
+            mTabLayout.addTab(mTabLayout.newTab().setIcon(FragmentGenerator.drawableArr[i]).setText(FragmentGenerator.strArr[i]));
         }
 
-        tabList.addItemDecoration(new MyDecoration(this, MyDecoration.HORIZONTAL_LIST));
-        XRecyclerViewAdapter adapter = new XRecyclerViewAdapter(this, mDataList);
-        adapter.setOnItemClickListener(this);
-        adapter.setOnItemLongClickListener(this);
-        tabList.setAdapter(adapter);
+//        tabList.addItemDecoration(new MyDecoration(this, MyDecoration.HORIZONTAL_LIST));
+//        XRecyclerViewAdapter adapter = new XRecyclerViewAdapter(this, mDataList);
+//        adapter.setOnItemClickListener(this);
+//        adapter.setOnItemLongClickListener(this);
+//        tabList.setAdapter(adapter);
+    }
+
+//    @Override
+//    public void onItemClick(int position) {
+//        Toast.makeText(getApplicationContext(), "onItemClick: " + position, Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    public void onItemLongClick(int position) {
+//        Toast.makeText(getApplicationContext(), "onItemLongClick: " + position, Toast.LENGTH_SHORT).show();
+//    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        int position = tab.getPosition();
+        onTabItemSelected(position);
+
+        for (int i = 0; i < mTabLayout.getTabCount(); i++) {
+            if (i == position) {
+                mTabLayout.getTabAt(i).setIcon(R.drawable.ic_launcher);
+            } else {
+                mTabLayout.getTabAt(i).setIcon(android.R.drawable.btn_dialog);
+            }
+        }
     }
 
     @Override
-    public void onItemClick(int position) {
-        Toast.makeText(getApplicationContext(), "onItemClick: " + position, Toast.LENGTH_SHORT).show();
+    public void onTabUnselected(TabLayout.Tab tab) {
+
     }
 
     @Override
-    public void onItemLongClick(int position) {
-        Toast.makeText(getApplicationContext(), "onItemLongClick: " + position, Toast.LENGTH_SHORT).show();
+    public void onTabReselected(TabLayout.Tab tab) {
+
+    }
+
+    private void onTabItemSelected(int position) {
+        Fragment fragment = null;
+
+        switch (position) {
+            case 0:
+                fragment = mFragmentList.get(0);
+                break;
+            case 1:
+                fragment = mFragmentList.get(1);
+                break;
+            case 2:
+                fragment = mFragmentList.get(2);
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.tab_container, fragment).commitAllowingStateLoss();
+        }
     }
 
     /**
---
+++ app/src/main/java/com/mrk/mrkplayer/model/FragmentGenerator.java
@@ -0,0 +1,24 @@
+package com.mrk.mrkplayer.model;
+
+import android.support.v4.app.Fragment;
+
+import com.mrk.mrkplayer.R;
+import com.mrk.mrkplayer.view.VideoFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentGenerator {
+    public static int[] strArr = new int[]{R.string.tab_video, R.string.tab_music, R.string.tab_dictionary};
+    public static int[] drawableArr = new int[]{R.drawable.tab_video, R.drawable.tab_music, R.drawable.tab_dictionary};
+
+    public static List<Fragment> getFragmentList() {
+        List<Fragment> fragmentList = new ArrayList<Fragment>();
+        fragmentList.add(new VideoFragment());
+        fragmentList.add(new VideoFragment());
+        fragmentList.add(new VideoFragment());
+
+        return fragmentList;
+    }
+
+}
