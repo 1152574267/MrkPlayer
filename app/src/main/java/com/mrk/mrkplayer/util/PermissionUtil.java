4d29afbee1e64ac1ae0fb3d02c3a0d9d4799020a
mrk <1152574267@qq.com>
2018-06-05 13:51:07 +0800

    Lint静态代码检测优化

---
+++ app/src/main/java/com/mrk/mrkplayer/util/PermissionUtil.java
@@ -0,0 +1,17 @@
+package com.mrk.mrkplayer.util;
+
+import android.Manifest;
+
+public class PermissionUtil {
+    public static final String[] REQUEST_PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.CHANGE_NETWORK_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE
+    };
+
+    public static final int REQUEST_PERMISSIONS_CODE = 100;
+}
