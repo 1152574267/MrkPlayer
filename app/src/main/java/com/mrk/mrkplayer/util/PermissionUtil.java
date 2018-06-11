0f26fa0cdd6e6f8d400fcf123a6fdd6a1cb7c8d3
mrk <1152574267@qq.com>
2018-06-11 18:21:17 +0800

    ffmpeg录制视频

--- app/src/main/java/com/mrk/mrkplayer/util/PermissionUtil.java
+++ app/src/main/java/com/mrk/mrkplayer/util/PermissionUtil.java
@@ -4,13 +4,16 @@ import android.Manifest;
 
 public class PermissionUtil {
     public static final String[] REQUEST_PERMISSIONS = {
+            Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.INTERNET,
             Manifest.permission.ACCESS_WIFI_STATE,
             Manifest.permission.ACCESS_NETWORK_STATE,
             Manifest.permission.CHANGE_NETWORK_STATE,
-            Manifest.permission.CHANGE_WIFI_STATE
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
     };
 
     public static final int REQUEST_PERMISSIONS_CODE = 100;
